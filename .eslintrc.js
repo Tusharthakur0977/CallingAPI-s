c7cde648494342aadf79f3f788d055a56746c027
Tushar Thakur <tusharthakur0977@gmail.com>
2024-04-10 19:23:06 +0530

    Wrote The API calling Code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: ['@react-native', 'prettier'],
+  rules: {
+    'prettier/prettier': 'on',
+    'react/no-unstable-nested-components': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
 };
+
--- App.tsx
+++ App.tsx
@@ -1,14 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-const App = () => {
-  return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+interface Post {
+  id?: number;
+  title?: string;
+  body?: string;
+  avatar?: any;
 }
 
-export default App
+const App: React.FC = () => {
 
-const styles = StyleSheet.create({})
+  const [data, setData] = useState<Post[]>([{
+    avatar: require('./assets/download.jpg')
+  }]);
+
+  // const gettApi = async () => {
+  //   const url = "https://jsonplaceholder.typicode.com/posts";
+  //   let result = await fetch(url);
+  //   result = await result.json();
+  //   setData(result);
+  // };
+
+
+  const gettApi = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    // try {
+      let response = await fetch(url);
+      let posts = await response.json();
+      const postsWithAvatar = posts.map(post => ({
+        ...post,
+        avatar: require('./assets/images.jpg')
+      }));
+      setData(postsWithAvatar);
+    // } catch (error) {
+      // console.error("Failed to fetch posts:", error);
+    // }
+  };
+
+  useEffect(() => {
+    gettApi();
+  }, []);
+
+  return (
+    <ScrollView>
+      <Text>App</Text>
+      {data.length ? data.map((item) =>
+        <View key={item.id}>
+          <Image style={{ height: 100, width: 100, }} source={item.avatar} />
+          <Text>{item.title}</Text>
+          <Text>{item.body}</Text>
+
+        </View>)
+        :
+        <Text>No Data Found</Text>}
+    </ScrollView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
